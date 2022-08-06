1c9e141d4eedce1294eacdf6b80ef18db89ce0d0
BioNomad <59677194+BioNomad@users.noreply.github.com>
2022-08-06 16:24:02 -0400

    Update ui_report.R

--- ui_report.R
+++ ui_report.R
@@ -2,14 +2,17 @@ fluidPage(
   fluidRow(
     box( 
       title = div(icon("file"),"Generate Report"),
-      width = 4,
+      width = 10,
       status = "primary",
       solidHeader = TRUE,
       radioButtons('format', 'Document format', c('PDF', 'HTML', 'Word'),
                    inline = TRUE),
       downloadButton("report","Generate Report",
                      icon = icon("play"),
-                     style="background-color: #301014;color:white;"))
+                     style="background-color: #301014;color:white;"),
+      br(),
+      downloadButton("downloadRes2","Download BRINDA Adjusted Data",
+                     style="background-color: #301014;color:white;align:right"))
   ),
   fluidRow(
     div(
