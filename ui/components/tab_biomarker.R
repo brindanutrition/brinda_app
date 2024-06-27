fbd4d7a2bd5f9e64b7510f635eddbba06b2e52ba
sf <shawnsfeng@gmail.com>
2024-06-27 03:43:52 -0700

    support conditional biomarker selection

--- ui/components/tab_biomarker.R
+++ ui/components/tab_biomarker.R
@@ -17,124 +17,147 @@ SmallPlotDivStyle <- SmallPlotDivStyle
 fluidPage(
   fluidRow(
     box(
-      title = div(icon("list"),"Select Biomarkers", style = MainBoxTitleStyle),
+      title = div(icon("list"), "Select Biomarkers", style = MainBoxTitleStyle),
       width = MainBoxWidth,
       status = "primary",
       solidHeader = TRUE,
       #######################
       div(
-        selectInput("ft","Ferritin",choices = ""),
-        style=SmallDropDownDivStyle),
+        selectInput("ft", "Ferritin", choices = ""),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        selectInput("ftU","Unit",choices=c("","\u03BCg/L","ng/mL")),
-        style=SmallDropDownDivStyle),
+        selectInput("ftU", "Unit", choices = c("", "\u03BCg/L", "ng/mL")),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        plotOutput("ftPlot",height = 115,width = 400),
-        style=SmallPlotDivStyle),
+        plotOutput("ftPlot", height = 115, width = 400),
+        style = SmallPlotDivStyle
+      ),
       br(),
       div(
-        selectInput("stfr","Soluble Transferrin Receptor",choices = ""),
-        style=SmallDropDownDivStyle),
+        selectInput("stfr", "Soluble Transferrin Receptor", choices = ""),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        selectInput("stfrU","Unit",choices=c("","mg/L")),
-        style=SmallDropDownDivStyle),
+        selectInput("stfrU", "Unit", choices = c("", "mg/L")),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        plotOutput("stfrPlot",height = 115,width = 400),
-        style=SmallPlotDivStyle),
+        plotOutput("stfrPlot", height = 115, width = 400),
+        style = SmallPlotDivStyle
+      ),
       br(),
       div(
-        selectInput("rt","Retinol",choices = ""),
-        style=SmallDropDownDivStyle),
+        selectInput("rt", "Retinol", choices = ""),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        selectInput("rtU","Unit",choices=c("","\u03BCmol/L","\u03BCg/dL")),
-        style=SmallDropDownDivStyle),
+        selectInput("rtU", "Unit", choices = c("", "\u03BCmol/L", "\u03BCg/dL")),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        plotOutput("rtPlot",height = 115,width = 400),
-        style=SmallPlotDivStyle),
+        plotOutput("rtPlot", height = 115, width = 400),
+        style = SmallPlotDivStyle
+      ),
       br(),
       div(
-        selectInput("rbp","Retinol Binding Protein",choices = ""),
-        style=SmallDropDownDivStyle),
+        selectInput("rbp", "Retinol Binding Protein", choices = ""),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        selectInput("rbpU","Unit",choices=c("","\u03BCmol/L","\u03BCg/dL")),
-        style=SmallDropDownDivStyle),
+        selectInput("rbpU", "Unit", choices = c("", "\u03BCmol/L", "\u03BCg/dL")),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        plotOutput("rbpPlot",height = 115,width = 400),
-        style=SmallPlotDivStyle),
+        plotOutput("rbpPlot", height = 115, width = 400),
+        style = SmallPlotDivStyle
+      ),
       br(),
       div(
-        selectInput("zn","Zinc",choices = ""),
-        style=SmallDropDownDivStyle),
+        selectInput("zn", "Zinc", choices = ""),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        selectInput("znU","Unit",choices=c("","\u03BCg/L","\u03BCmol/L")),
-        style=SmallDropDownDivStyle),
+        selectInput("znU", "Unit", choices = c("", "\u03BCg/L", "\u03BCmol/L")),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        plotOutput("znPlot",height = 115,width = 400),
-        style=SmallPlotDivStyle),
+        plotOutput("znPlot", height = 115, width = 400),
+        style = SmallPlotDivStyle
+      ),
       br(),
       div(
-        selectInput("agp","AGP *",choices = ""),
-        style=SmallDropDownDivStyle),
+        selectInput("agp", "AGP *", choices = ""),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        selectInput("agpU","Unit *",choices=c("","g/L")),
-        style=SmallDropDownDivStyle),
+        selectInput("agpU", "Unit *", choices = c("", "g/L")),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        plotOutput("agpPlot",height = 115,width = 400),
-        style=SmallPlotDivStyle),
+        plotOutput("agpPlot", height = 115, width = 400),
+        style = SmallPlotDivStyle
+      ),
       br(),
       div(
-        selectInput("crp","CRP *",choices = ""),
-        style=SmallDropDownDivStyle),
+        selectInput("crp", "CRP *", choices = ""),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        selectInput("crpU","Unit *",choices=c("","mg/L")),
-        style=SmallDropDownDivStyle),
+        selectInput("crpU", "Unit *", choices = c("", "mg/L")),
+        style = SmallDropDownDivStyle
+      ),
       div(
-        plotOutput("crpPlot",height = 115,width = 400),
-        style=SmallPlotDivStyle),
+        plotOutput("crpPlot", height = 115, width = 400),
+        style = SmallPlotDivStyle
+      ),
       br(),
       #######################
       div(
-        selectInput("fastingVar",
-                    "What is the general fasting status of your data? *",
-                    choices = c("fasted","non-fasted","unknown")),
-        style=LargeDropDownDivStyle
+        selectInput("pop", "Population *", choices = c(
+          "",
+          "Women of Reproductive Age",
+          "Preschool-age children", "Other (non-WRA, non-PSC population group)",
+          "User-defined AGP and CRP cutoffs"
+        )),
+        style = MediumDropDownDivStyle
       ),
       br(),
       div(
-        selectInput("timeVar",
-                    "What is the general time of blood draw for your data? *",
-                    choices = c("morning",
-                                "afternoon",
-                                "evening",
-                                "unknown")),
-        style=LargeDropDownDivStyle
+        selectInput(
+          "bloodDrawSameTime",
+          "Were the blood draw for every participant
+          taken at the same time of day? *",
+          choices = c("yes", "no"),
+        ),
+        style = LargeDropDownDivStyle
       ),
-      br(),
-      div(
-        selectInput("pop","Population *",choices = c("",
-                                                     "Women of Reproductive Age", 
-                                                     "Preschool-age children", "Other (non-WRA, non-PSC population group)",
-                                                     "User-defined AGP and CRP cutoffs")),
-        style=MediumDropDownDivStyle),
-      br(),
+      uiOutput("sameBloodDrawFastingVarSelection"),
+      uiOutput("sameBloodDrawTimeVarSelection"),
+      uiOutput("differentBloodDrawFastingVarColumnName"),
+      uiOutput("differentBloodDrawTimeVarColumnName"),
       uiOutput("manAgp"),
       uiOutput("manCrp"),
-      br(),
       br()
     )
   ),
   fluidRow(
     div(
-      style=FooterNavigationButtonStyle,
+      style = FooterNavigationButtonStyle,
       actionButton("clickPrevOnBiomarkerPage",
-                   "Previous",
-                   icon = icon("backward"), 
-                   style=ButtonStyle)),
+        "Previous",
+        icon = icon("backward"),
+        style = ButtonStyle
+      )
+    ),
     div(
-      style=FooterNavigationButtonStyle,
+      style = FooterNavigationButtonStyle,
       actionButton("clickNextOnBiomarkerPage",
-                   "Next",
-                   icon = icon("forward"), 
-                   style=ButtonStyle))
+        "Next",
+        icon = icon("forward"),
+        style = ButtonStyle
+      )
+    )
   )
 )
